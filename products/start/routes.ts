c9719d8f33457e3bb08d58154d93cb975b2b6d58
mayachim <myabualhaija@gmail.com>
2023-02-21 01:45:16 +0300

    routes update

--- products/start/routes.ts
+++ products/start/routes.ts
@@ -14,6 +14,14 @@ Route.group(()=>{
 }).prefix('/actors')
 
 Route.group(()=>{
+  Route.post('/',  "AddressesController.create")
+  Route.put('/',  "AddressesController.update")
+  Route.delete('/:id',  "AddressesController.destory")
+  Route.get('/:id',  "AddressesController.getById")
+  Route.get('/',  "AddressesController.getAll") 
+}).prefix('/addresses')
+
+Route.group(()=>{
   Route.post('/',  "CategoriesController.create")
   Route.put('/',  "CategoriesController.update")
   Route.delete('/:id',  "CategoriesController.destory")
@@ -22,12 +30,76 @@ Route.group(()=>{
 }).prefix('/categories')
 
 Route.group(()=>{
+  Route.post('/',  "CitiesController.create")
+  Route.put('/',  "CitiesController.update")
+  Route.delete('/:id',  "CitiesController.destory")
+  Route.get('/:id',  "CitiesController.getById")
+  Route.get('/',  "CitiesController.getAll") 
+}).prefix('/cities')
+
+Route.group(()=>{
   Route.post('/',  "CountriesController.create")
   Route.put('/',  "CountriesController.update")
   Route.delete('/:id',  "CountriesController.destory")
   Route.get('/:id',  "CountriesController.getById")
   Route.get('/',  "CountriesController.getAll") 
 }).prefix('/countries')
+
+Route.group(()=>{
+  Route.post('/',  "CustomersController.create")
+  Route.put('/',  "CustomersController.update")
+  Route.delete('/:id',  "CustomersController.destory")
+  Route.get('/:id',  "CustomersController.getById")
+  Route.get('/',  "CustomersController.getAll") 
+}).prefix('/customers')
+
+Route.group(()=>{
+  Route.post('/',  "FilmsController.create")
+  Route.put('/',  "FilmsController.update")
+  Route.delete('/:id',  "FilmsController.destory")
+  Route.get('/:id',  "FilmsController.getById")
+  Route.get('/',  "FilmsController.getAll") 
+}).prefix('/films')
+
+Route.group(()=>{
+  Route.post('/',  "InventoriesController.create")
+  Route.put('/',  "InventoriesController.update")
+  Route.delete('/:id',  "InventoriesController.destory")
+  Route.get('/:id',  "InventoriesController.getById")
+  Route.get('/',  "InventoriesController.getAll") 
+}).prefix('/inventories')
+
+Route.group(()=>{
+  Route.post('/',  "LanguagesController.create")
+  Route.put('/',  "LanguagesController.update")
+  Route.delete('/:id',  "LanguagesController.destory")
+  Route.get('/:id',  "LanguagesController.getById")
+  Route.get('/',  "LanguagesController.getAll") 
+}).prefix('/languages')
+
+Route.group(()=>{
+  Route.post('/',  "PaymentsController.create")
+  Route.put('/',  "PaymentsController.update")
+  Route.delete('/:id',  "PaymentsController.destory")
+  Route.get('/:id',  "PaymentsController.getById")
+  Route.get('/',  "PaymentsController.getAll") 
+}).prefix('/payments')
+
+Route.group(()=>{
+  Route.post('/',  "RentalsController.create")
+  Route.put('/',  "RentalsController.update")
+  Route.delete('/:id',  "RentalsController.destory")
+  Route.get('/:id',  "RentalsController.getById")
+  Route.get('/',  "RentalsController.getAll") 
+}).prefix('/rentals')
+
+Route.group(()=>{
+  Route.post('/',  "StaffController.create")
+  Route.put('/',  "StaffController.update")
+  Route.delete('/:id',  "StaffController.destory")
+  Route.get('/:id',  "StaffController.getById")
+  Route.get('/',  "StaffController.getAll") 
+}).prefix('/staff')
 
 Route.group(()=>{
   Route.post('/',  "StoresController.create")
